d48b1b377cafd8ed4198b05b310b13f49118b930
Ivan Saucedo <ivan.scdo@gmail.com>
2018-03-04 23:14:01 -0600

    mb su304.23:11 v1.2 push stills to page v1.3 button spacing, sticky header & sidebar

--- dev/dev.js
+++ dev/dev.js
@@ -58,17 +58,70 @@ $( document ).ready(function() {
     //     gifButton.attr("id", searchTerm);
     // });
 
-    var topics = ["volkswagen", "cooking", "poetry", "radiohead", "kendrick lamar", "the beatles", "olde english bulldogge", "pekingese", "corgi", "simpsons", "futurama", "rick and morty", "pokemon", "final fantasy tactics", "breath of the wild"];
+    var topics = ["volkswagen", "cooking", "poetry", "radiohead", "kendrick lamar", "the beatles", "olde english bulldogge", "pekingese", "corgi", "simpsons", "mr. robot", "rick and morty", "pokemon", "chrono trigger", "breath of the wild"];
 
     for ( let i = 0; i < topics.length; i++) {
         var gifButton = $("<button>");
         gifButton.text(topics[i]);
         gifButton.appendTo("#jean-jacket");
-        gifButton.attr("class", "btn btn-outline-danger");
+        // gifButton.attr("class", "btn btn-outline-danger gif-button");
+        gifButton.attr("class", "btn btn-info btn-space gif-button");
         gifButton.attr("id", topics[i]);
     }
 
-    
+    $(".gif-button").on("click", function(){
+
+        var queryURL = "https://api.giphy.com";
+        var searchEndPoint = "/v1/gifs/search" + "?rating=g&q=";
+        var searchTerm = $(this).attr("id");
+        var apiKey = "&api_key=" + "7gCY693a53ZjIQPAsatsvX9jwLXFFcNe";
+        var limit = "&limit=10";
+
+        queryURL += [
+            searchEndPoint +
+            searchTerm +
+            apiKey +
+            limit
+        ];
+
+        var dotGet = $.get(queryURL);
+
+        for(let i = 0; i < 10; i++) {
+            dotGet.then(function(response) { 
+                // console.log("red wine, success! ", response);
+                // console.log(response.data[i].url); 
+                var gifURL = response.data[i].url;
+                var gifRating = response.data[i].rating;
+                // var gifStillURL = response.data[i].images.fixed_height_still.url;
+                var gifStillURL = response.data[i].images.fixed_width_still.url;
+                // console.log(gifStillURL);
+                // console.log(response.data[i].images)
+                var gifStill = $("<img>");
+                gifStill.attr("src", gifStillURL);
+                $(gifStill).prependTo("div#album");
+            });
+
+        
+
+        
+
+
+
+
+        // END OF: for(let i = 0; i < 10; i++) {    
+        }
+
+
+
+
+
+
+
+    // END OF: $(".gif-button").on("click", function(){
+    })
+
+
+
     
 
     
